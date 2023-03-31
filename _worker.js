bab9ffe66deffca57bbc32e72d22f68f4d976f45
yuan8ba <41765558+yuan8ba@users.noreply.github.com>
2023-03-31 18:47:28 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,13 +2,9 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     var reg = RegExp(/qq/);
-    if(reg.exec(request.url)){
-      url.hostname = 'usqm6z.eu-eejfutefcmzq.dopraxrocks.com'
-      let new_request = new Request(url, request);
-      return fetch(new_request);
-    }
+    
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'usqm6z.eu-eejfutefcmzq.dopraxrocks.com'
+      url.hostname = 'usce9b.eu-ffaqrvbjixds.dopraxrocks.net'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
